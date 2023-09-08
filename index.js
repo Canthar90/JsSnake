b4661071e7ab68e459b923f1bce3481127537a9d
Canthar90 <d.cieslak90@gmail.com>
2023-09-08 13:02:41 +0200

    Added basic bottom colision

--- index.js
+++ index.js
@@ -3,23 +3,37 @@ let direction
 let board 
 let boardPos 
 let boardTop
+let boardLeft
+let snakeHead = document.getElementById("snake-segment-1")
+
+const boardHight = 900
+const boardWidth = 1200
+
+const rem = parseFloat(getComputedStyle(document.documentElement).fontSize)
+
 board = document.getElementById('game-board-1')
-boardTop = board.style.getPropertyValue("top")
+boardTop = window.getComputedStyle(board).getPropertyValue("top").replace("px", "")
+boardLeft = window.getComputedStyle(board).getPropertyValue("left").replace("px", "")
+
+
 
 if(!interval){
-    interval = setInterval(gameLogic, 250)
-    
-    console.log(board)
-    
+    interval = setInterval(gameLogic, 200)
+   
 }
 
 
 function moveDown(element){
-    console.log(boardTop)
+    
     let elemStyle = window.getComputedStyle(element)
     let topValue = elemStyle.getPropertyValue("top").replace("px", "")
+    
+    let bottomValue = Number(topValue) + 2 * Number(rem)
+    
+    if (bottomValue <= boardHight) {
+        element.style.top = (Number(topValue) + 10) + "px"
+    }
    
-    element.style.top = (Number(topValue) + 10) + "px"
 }
 
 function moveUp(element){
@@ -45,16 +59,16 @@ function moveLeft(element){
 
 function moveDirections() {
     if (direction === "down"){
-        let snakeHead = document.getElementById("snake-segment-1")
+        
         moveDown(snakeHead)
     }else if(direction === "up"){
-        let snakeHead = document.getElementById("snake-segment-1")
+    
         moveUp(snakeHead)
     }else if(direction === "right"){
-        let snakeHead = document.getElementById("snake-segment-1")
+     
         moveRight(snakeHead)
     }else if(direction === "left"){
-        let snakeHead = document.getElementById("snake-segment-1")
+     
         moveLeft(snakeHead)
     }else{
         return
