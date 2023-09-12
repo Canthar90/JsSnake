610bae35b80ed9336589d18b6041e032e58c01a8
Canthar90 <d.cieslak90@gmail.com>
2023-09-12 13:51:49 +0200

    Added score and new segment generation + some multi segment move

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ let boardPos
 let boardTop
 let boardLeft
 let snakeHead = document.getElementById("snake-segment-1")
+let snakeSegments = [snakeHead]
+let score = 0
 
 const boardHight = 900
 const boardWidth = 1200
@@ -18,8 +20,19 @@ boardLeft = window.getComputedStyle(board).getPropertyValue("left").replace("px"
 
 
 if(!interval){
-    interval = setInterval(gameLogic, 200)
+    interval = setInterval(gameLogic, 450)
    
+}
+
+function newSnakeElement() {
+    score ++
+    
+    let newElement = snakeSegments[0].cloneNode(true)
+    newElement.id = `snake-element-${score}`
+    console.log(newElement.id)
+    snakeSegments.push(newElement)
+    console.log(snakeSegments)
+    snakeHead.after(newElement)
 }
 
 
@@ -28,10 +41,15 @@ function moveDown(element){
     let elemStyle = window.getComputedStyle(element)
     let topValue = elemStyle.getPropertyValue("top").replace("px", "")
     
+    let leftCorrectionValue = elemStyle.getPropertyValue("left")
+    
     let bottomValue = Number(topValue) + 2 * Number(rem)
+    let elementToMove = snakeSegments.pop()
     
     if (bottomValue < boardHight - Number(boardTop)) {
-        element.style.top = (Number(topValue) + 10) + "px"
+        elementToMove.style.top = (Number(topValue) + 2*Number(rem)) + "px"
+        elementToMove.style.left = leftCorrectionValue
+        snakeSegments.unshift(elementToMove)
     }
    
 }
@@ -40,8 +58,14 @@ function moveUp(element){
     let elementStyle = window.getComputedStyle(element)
     let topValue = elementStyle.getPropertyValue("top").replace("px","")
 
+    let leftCorrectionValue = elementStyle.getPropertyValue("left")
+
+    let elementToMove = snakeSegments.pop()
+
     if(Number(topValue) >= Number(boardTop)){
-        element.style.top = (Number(topValue) - 10) + "px"
+        elementToMove.style.top = (Number(topValue) - 2*Number(rem)) + "px"
+        elementToMove.style.left = leftCorrectionValue
+        snakeSegments.unshift(elementToMove)
     }
     
 }
@@ -50,10 +74,15 @@ function moveRight(element){
     let elementStyle = window.getComputedStyle(element)
     let leftValue = elementStyle.getPropertyValue("left").replace("px","")
 
+    let topCorrectionValue = elementStyle.getPropertyValue("top")
+
     let rightValue = Number(leftValue) + 2 * Number(rem)
+    let elementToMove = snakeSegments.pop()
     
     if (rightValue <= boardWidth - Number(boardLeft)){
-        element.style.left = (Number(leftValue) + 10) + "px"
+        elementToMove.style.left = (Number(leftValue) + 2*Number(rem)) + "px"
+        elementToMove.style.top = topCorrectionValue
+        snakeSegments.unshift(elementToMove)
     }
 
     
@@ -62,25 +91,34 @@ function moveRight(element){
 function moveLeft(element){
     let elementStyle = window.getComputedStyle(element)
     let leftValue = elementStyle.getPropertyValue("left").replace("px","")
+
+    let topCorrectionValue = elementStyle.getPropertyValue("top")
+
+    let elementToMove = snakeSegments.pop()
+
     if (Number(leftValue) >= Number(boardLeft)){
-        element.style.left = (Number(leftValue) - 10) + "px"
+        elementToMove.style.left = (Number(leftValue) - 2*Number(rem)) + "px"
+        elementToMove.style.top = topCorrectionValue
+        snakeSegments.unshift(elementToMove)
     }
     
 }
 
 function moveDirections() {
+    let snakeFirtsSegment = snakeSegments[0]
+    
     if (direction === "down"){
         
-        moveDown(snakeHead)
+        moveDown(snakeFirtsSegment)
     }else if(direction === "up"){
     
-        moveUp(snakeHead)
+        moveUp(snakeFirtsSegment)
     }else if(direction === "right"){
      
-        moveRight(snakeHead)
+        moveRight(snakeFirtsSegment)
     }else if(direction === "left"){
      
-        moveLeft(snakeHead)
+        moveLeft(snakeFirtsSegment)
     }else{
         return
     }
@@ -90,16 +128,16 @@ function moveDirections() {
 function gameLogic () {
     document.addEventListener('keydown', (event)=>{
         
-        if (event.key === "ArrowDown" && direction !== 'down'){
+        if (event.key === "ArrowDown" && direction !== 'down' && direction !== "up"){
             direction = "down"
             
-        }else if (event.key === "ArrowUp" && direction !== "up"){
+        }else if (event.key === "ArrowUp" && direction !== "up" && direction !== "down"){
             direction = "up"
             
-        }else if (event.key === "ArrowLeft" && direction !== "left"){
+        }else if (event.key === "ArrowLeft" && direction !== "left" && direction !== "right"){
             direction = "left"
             
-        }else if (event.key === "ArrowRight" && direction !== "right"){
+        }else if (event.key === "ArrowRight" && direction !== "right" && direction !== "left"){
             direction = "right"
             
         }
@@ -108,4 +146,7 @@ function gameLogic () {
 
     moveDirections()
 }
+
+newSnakeElement()
+newSnakeElement()
 
